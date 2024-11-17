3c3edd9fa324d9b930c90372d414be18e42800cf
Vitalii S. <svitalii138@gmail.com>
2024-11-18 00:01:14 +0200

    voice message restriction added

--- bot.js
+++ bot.js
@@ -15,8 +15,35 @@ bot.on("text", (ctx) => {
   }
 });
 
+const userAttempts = {};
+
+bot.on("voice", (ctx) => {
+  const userId = ctx.message.from.id;
+
+  ctx.deleteMessage();
+
+  // Проверяем, сколько раз пользователь отправлял голосовые сообщения
+  if (userAttempts[userId]) {
+    userAttempts[userId]++;
+  } else {
+    userAttempts[userId] = 1;
+  }
+
+  // Если пользователь отправил голосовое сообщение 2-й раз
+  if (userAttempts[userId] === 2) {
+    ctx.reply("Вы снова отправили голосовое сообщение. Пожалуйста, следуйте правилам!");
+  } else {
+    ctx.reply("Голосовые сообщения запрещены в этой группе. Пожалуйста, отправьте текст.");
+  }
+
+  // Сбросим попытки после 2-х повторений
+  setTimeout(() => {
+    userAttempts[userId] = 0;
+  }, 60000); // Через 1 минуту можно будет отправить голосовое сообщение без ограничений
+});
+
 bot.on("new_chat_members", (ctx) => {
   ctx.reply("Добро пожаловать в группу! Пожалуйста, соблюдайте правила.");
 });
 
-bot.launch().then(() => console.log("Бот запущен и фильтрует спам!"));
+bot.launch().then(() => console.log("Bot is running"));
